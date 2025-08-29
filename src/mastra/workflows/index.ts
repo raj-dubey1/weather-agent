6c1671bcf35d6315761dda8bed9fab137a1ebea9
raj.CometChat <92921875+raj-dubey1@users.noreply.github.com>
2025-08-29 13:50:09 +0530

    Update index.ts

--- src/mastra/workflows/index.ts
+++ src/mastra/workflows/index.ts
@@ -3,7 +3,7 @@ import { Agent } from '@mastra/core/agent';
 import { createStep, createWorkflow } from '@mastra/core/workflows';
 import { z } from 'zod';
 
-const llm = openai('gpt-4o');
+const llm = openai('gpt-4o-mini');
 
 const agent = new Agent({
   name: 'Weather Agent',
