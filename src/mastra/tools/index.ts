a5338b27d5f5f2e7cc2137ac4d60510744823338
raj.CometChat <92921875+raj-dubey1@users.noreply.github.com>
2025-09-24 15:33:44 +0530

    Update index.ts

--- src/mastra/tools/index.ts
+++ src/mastra/tools/index.ts
@@ -44,7 +44,7 @@ export const metadataTool = createTool({
   id: 'get-metadata',
   description: 'Get metadata',
   execute: async ({ context, runtimeContext }) => {
-    return runtimeContext.get('cometchatContext');
+    return `The metadata is ${JSON.stringify(runtimeContext.get('cometchatContext')))}`;
   },
 });
 
