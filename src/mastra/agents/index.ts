d048c3c59cff4ea5ca19f2c63eb4726a3826c99a
raj.CometChat <92921875+raj-dubey1@users.noreply.github.com>
2025-09-24 15:01:20 +0530

    Update index.ts

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,6 +1,6 @@
 import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
-import { weatherTool } from '../tools';
+import { weatherTool,metadataTool } from '../tools';
 
 export const weatherAgent = new Agent({
   name: 'Weather Agent',
@@ -17,5 +17,5 @@ export const weatherAgent = new Agent({
       Use the weatherTool to fetch current weather data.
 `,
   model: openai('gpt-4o-mini'),
-  tools: { weatherTool },
+  tools: { weatherTool,metadataTool },
 });
